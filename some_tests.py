fc5360476fdfcd971715a48bad24dfd4e2ddf033
Nico Piatkowski <np84>
2022-01-21 19:33:44 +0100

    update code

--- some_tests.py
+++ some_tests.py
@@ -6,14 +6,19 @@ np.set_printoptions(threshold=sys.maxsize,linewidth=1024)
 import itertools
 from colored import fg, bg, attr
 
-from qiskit.opflow import I, X, Z, MatrixOp
+from qiskit.opflow import I, X, Z, Plus, Minus, H, Zero, One
 from qiskit.compiler import transpile
-from qiskit.quantum_info.states.statevector import Statevector
-from qiskit.quantum_info import partial_trace, DensityMatrix
+#from qiskit.quantum_info.states.statevector import Statevector
+#from qiskit.quantum_info import partial_trace, DensityMatrix
+#
+from qiskit import QuantumRegister, QuantumCircuit
+from qiskit import Aer, assemble
+
+
 
 ####################################### input structure
 
-C = [[0,1],[1,2]] # clique structure, 2-var chain
+C = [[0,1]] # clique structure, 2-var chain
 #C = [[0,1],[1,2],[2,3],[0,3]] # clique structure, 4-var circle
 
 n = len(np.unique(np.array(C).flatten())) # number of (qu)bits
@@ -25,8 +30,6 @@ for c in C:
 dim = 2**n
 
 ####################################### utility funcs
-
-h = MatrixOp((1.0/np.sqrt(2.0)) * np.array([[1,1],[1,-1]])) # Hadamard gate
 
 def grouped(iterable, n):
 	return zip(*[iter(iterable)]*n)
@@ -85,19 +88,19 @@ def genPhi(c,y):
 
 def genHamiltonian():
 	L = []
-	H = 0
+	R = 0
 	i = 0
 	for l,c in enumerate(C):
 		Ly = []
 		for r,y in enumerate(list(itertools.product([0, 1], repeat=len(c)))):
 			Phi = genPhi(c,y)
-			theta = -0.1*(i+1) #np.random.uniform() # we need a negative MRF
-			H += Phi * -theta
+			theta = -1*(i+1) #np.random.uniform() # we need a negative MRF
+			R += Phi * -theta
 			Ly.append((theta,Phi)) # list of all factors that belong to same clique
 			i = i + 1
 		L.append(Ly) # list of factor-lists, one factor-list per clique
 
-	return H,L
+	return R,L
 
 def genPhaseFactors(ey):
 	t = np.sqrt((-1-ey)/(-1+ey))
@@ -108,10 +111,9 @@ def genPhaseFactors(ey):
 	return np.array([phi1, phi2])
 	
 def genUphi(U,phi):
-	M  = Z^(I^n) # (2PI-(I^n))
-	F1 = (-phi[0] * M).exp_i()
-	F2 = (-phi[1] * M).exp_i()
-	return F1 @ (~U) @ F2 @ U
+	RZ1 = (phi[0] * Z).exp_i() ^ (I^n) # ignored sign of phi
+	RZ2 = (phi[1] * Z).exp_i() ^ (I^n) # ignored sign of phi
+	return RZ2 @ U @ RZ2 @ U # ignored conjugate transpose of first U
 
 # works only if Eigenvalues of A are bounded by 1	
 def uniEmbedding(A):
@@ -122,42 +124,72 @@ def conjugateBlocks(A):
 	return (((I+Z)/2)^A) + (((I-Z)/2)^(~A))
 
 # works only if number of cliques is a power of two
-def expH_block_factors_from_list(beta, L0, lnZ=0):
+def expH_from_list_blocked(beta, L0, lnZ=0):
 	R = []
 	for Ly in L0:
 		L = [genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*(w0 - (lnZ/len(C)))))) for w0,Phi0 in Ly]
 		R.append(merge_all(L))
 	M = merge_all(R)
-	O = h^(I^int(n+1+np.log2(d)))
+	O = H^(I^int(n+1+np.log2(d)))
 	return O @ conjugateBlocks(M) @ (~O)
 	
-def expH_from_list(beta, L0, lnZ=0, forceReal=False):
+def expH_from_list_algebraic_real(beta, L0, lnZ=0):
 	RESULT = I^(n+1)
 	for L in L0:
 		for (w0,Phi0) in L:
 			U = genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*(w0 - (lnZ/len(C))))))
-			if forceReal:
-				RESULT = ((U+(~U))/2) @ RESULT # non-unitary extraction of real part
-			else:
-				RESULT = U @ RESULT # unitary extraction of real part
+			RESULT = (U+(~U))/2 @ RESULT # non-unitary extraction of real part
 	return RESULT
 	
+def expH_from_list_unreal(beta, L0, lnZ=0):
+	RESULT = I^(n+1)
+	#if not forceReal:
+	#	RESULT = I^(n+1+d) # one ancille per factor
+	for L in L0:
+		for (w0,Phi0) in L:
+			U = genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*(w0 - (lnZ/len(C))))))
+			RESULT = U @ RESULT # unitary extraction of real part
+	return RESULT
+
+def expH_from_list_real_RUL(beta, L0, lnZ=0):
+	qr = QuantumRegister(n+1+d, 'q') # one ancille per factor
+	circ = QuantumCircuit(n+1+d,n+1+d)
+	for i in range(n+1):
+		circ.h(qr[i])
+	
+	i = 0 # enumerate 0..d-1
+	for ii,L in enumerate(L0):
+		for jj,(w0,Phi0) in enumerate(L):
+			U = genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*(w0 - (lnZ/len(C))))))
+			
+			u = conjugateBlocks(U).to_circuit().to_instruction(label='U_C'+str(ii)+'_y'+str(jj))
+			
+			circ.h(qr[n+1+i])
+			circ.append(u, [qr[j] for j in range(n+1)]+[qr[n+1+i]])
+			circ.h(qr[n+1+i])
+			
+			i = i + 1
+			
+	circ.measure([qr[j] for j in range(n+1+d)],range(n+1+d))
+	return circ
+
 #######################################
 
-H,L  = genHamiltonian() # L is list of factors
+HAM,L  = genHamiltonian() # L is list of factors
 beta = 1
+lnZ = np.log(np.trace(expm(-beta*HAM.to_matrix())))
 
-R0   = expm(-beta*H.to_matrix()) # exp(-βH) via numpy for debugging
+R0   = expm(-beta*HAM.to_matrix()) # exp(-βH) via numpy for debugging
 
 #######################################
 
 print("NODES ("+format_math('n')+"):",n," PARAMETERS ("+format_math('d')+"):",d)
 
 #######################################
-print(format_head("CHECK 1","expH_from_list(.., forceReal=True);"))
+print(format_head("CHECK 1","expH_from_list_algebraic_real(..);"))
 print("  * This shows that Eq.(3) from the draft works.")
 
-R1   = expH_from_list(beta, L, forceReal=True)
+R1   = expH_from_list_algebraic_real(beta, L)
 r1   = R1.to_matrix()
 
 print("  * "+format_math('l2')+"-Error between",format_math('exp(-βH)'),"and upper left")
@@ -165,29 +197,36 @@ print("    block of",format_math('PROD_j REAL(U^j_(γ^y))')+":", format_val(np.l
 
 #######################################
 print()
-print(format_head("CHECK 2","expH_from_list(.., forceReal=False);"))
+print(format_head("CHECK 2","expH_from_list_unreal(..);"))
 print("  * This shows that Eq. (3) from the draft does *NOT*")
 print("    work when we do not extract real parts.")
 
-R2   = expH_from_list(beta, L, forceReal=False)
+R2   = expH_from_list_unreal(beta, L)
 r2   = R2.to_matrix()
 
 print("  * "+format_math('l2')+"-Error between",format_math('exp(-βH)'),"and upper left")
 print("    block of",format_math('PROD_j U^j_(γ^y)')+":", format_val(np.linalg.norm(R0 - r2[:dim,:dim], ord=2)))
 
-#p = np.diag(R0)
-#q = np.diag(r2)[:2**n]
-#
-#print(p)
-#print(q)
-
 #######################################
 print()
-print(format_head("CHECK 3","expH_block_factors_from_list(..);"))
+print(format_head("CHECK 2b","expH_from_list_real_RUL(..);"))
+print("  * Eq. (3) with RUL")
+
+R2b   = expH_from_list_real_RUL(beta, L, lnZ)
+print(R2b)
+r2b   = R2.to_matrix()
+
+print("  * "+format_math('l2')+"-Error between",format_math('exp(-βH)'),"and upper left")
+print("    block of",format_math('PROD_j U^j_(γ^y)')+":", format_val(np.linalg.norm(R0 - r2[:dim,:dim], ord=2)))
+
+#######################################
+
+print()
+print(format_head("CHECK 3","expH_from_list_blocked(..);"))
 print("  * This shows that the new block factorization with ("+format_math('1+log_2(d)'))
 print("    extra ancillas) has correct values of each factor on the diagonal.")
 
-R3   = expH_block_factors_from_list(beta, L)
+R3   = expH_from_list_blocked(beta, L)
 r3   = R3.to_matrix()
 
 r = np.eye(2*dim) # 2*dim = 2**(n+1)
@@ -224,3 +263,19 @@ UU = transpile(Q.to_circuit(), basis_gates=['cx','id','rz','sx','x'], optimizati
 print("  * Number of gates:", format_val(len(UU)))
 print("  * Depth:", format_val(UU.depth()))
 
+#######################################
+print()
+print(format_head("CHECK 6","FULL CIRCUIT"))
+print("  * This shows size of circuit from CHECK 2b.")
+
+UU = transpile(R2b, basis_gates=['cx','id','rz','sx','x'], optimization_level=3)
+
+print("  * Number of gates:", format_val(len(UU)))
+print("  * Depth:", format_val(UU.depth()))
+
+#######################################
+print()
+print(format_head("CHECK 7","SIMULATION"))
+
+sim = Aer.get_backend('aer_simulator')
+j = sim.run(assemble(UU,shots=100000))
