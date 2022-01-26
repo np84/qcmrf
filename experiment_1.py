485f65562cec5cdc7e3f0257c12a0346b6a5bf50
Nico Piatkowski <nico.piatkowski@iais.fraunhofer.de>
2022-01-26 20:47:02 +0100

    minor changes

--- experiment_1.py
+++ experiment_1.py
@@ -21,32 +21,32 @@ def fidelity(P,Q):
 
 def grouped(iterable, n):
 	return zip(*[iter(iterable)]*n)
-	
+
 def merge(L):
 	R = []
 	for (M0,M1) in grouped(L,2):
 		R.append((((I+Z)/2)^M0) + (((I-Z)/2)^M1))
 	return R
-	
+
 def merge_all(L):
 	R = L
 	while len(R)>1:
 		R = merge(R)
 	return R[0]
-	
+
 turquoise = '#179c7d'
 orange = '#eb6a0a'
 blue = '#1f82c0'
-	
+
 def format_head(h,t):
 	return fg(turquoise)+attr('bold')+h+attr('reset')+': '+fg('white')+attr('bold')+t+attr('reset')
-	
+
 def format_val(val):
 	if type(val) is int:
 		return fg('white')+attr('bold')+str(val)+attr('reset')
 	else:
 		return fg('white')+attr('bold')+np.format_float_scientific(val, precision=4)+attr('reset')
-		
+
 def format_math(math):
 	brackets  = ['(',')']
 	operators = ['+','-','*','/','_','^']
@@ -63,7 +63,7 @@ def format_math(math):
 
 def genPhi(c,y):
 	result = 1
-	
+
 	plus  = [v for i,v in enumerate(c) if not y[i]] # 0
 	minus = [v for i,v in enumerate(c) if     y[i]] # 1
 
@@ -75,7 +75,7 @@ def genPhi(c,y):
 			f = (I+Z)/2
 
 		result = result^f
-		
+
 	return result
 
 def genHamiltonian():
@@ -96,22 +96,22 @@ def genHamiltonian():
 
 def genPhaseFactors(ey):
 	t = np.sqrt((-1-ey)/(-1+ey))
-	
+
 	phi1 = np.angle( ( (1+t*1j) + ey*(1-t*1j) ) / 2 )
 	phi2 = np.real(np.angle( np.sqrt(0.5) * np.sqrt(1 - ey) * (t + 1j) ))
-	
+
 	return np.array([phi1, phi2])
-	
+
 def genUphi(U,phi):
 	RZ1 = (phi[0] * Z).exp_i() ^ (I^n) # ignored sign of phi
 	RZ2 = (phi[1] * Z).exp_i() ^ (I^n) # ignored sign of phi
 	return RZ2 @ U @ RZ2 @ U # ignored conjugate transpose of first U
 
-# works only if Eigenvalues of A are bounded by 1	
+# returns unitary if Eigenvalues of A are bounded by 1
 def uniEmbedding(A):
 	return (X^((I^n)-A)) + (Z^A)
 
-# returns unitary if A is unitary	
+# returns unitary if A is unitary
 def conjugateBlocks(A):
 	return (((I+Z)/2)^A) + (((I-Z)/2)^(~A))
 
@@ -124,7 +124,7 @@ def expH_from_list_blocked(beta, L0, lnZ=0):
 	M = merge_all(R)
 	O = H^(I^int(n+1+np.log2(d)))
 	return O @ conjugateBlocks(M) @ (~O)
-	
+
 def expH_from_list_real_algebraic(beta, L0, lnZ=0):
 	RESULT = I^(n+1)
 	for L in L0:
@@ -132,43 +132,44 @@ def expH_from_list_real_algebraic(beta, L0, lnZ=0):
 			U = genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*(w0 - (lnZ/len(C))))))
 			RESULT = (U+(~U))/2 @ RESULT # non-unitary extraction of real part
 	return RESULT
-	
+
 def expH_from_list_unreal(beta, L0, lnZ=0):
 	RESULT = I^(n+1)
 	for L in L0:
 		for (w0,Phi0) in L:
 			U = genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*(w0 - (lnZ/len(C))))))
-			RESULT = U @ RESULT # unitary extraction of real part
+			RESULT = U @ RESULT # complex result
 	return RESULT
 
 def expH_from_list_real_RUS(beta, L0, lnZ=0):
-	qr = QuantumRegister(n+1+d, 'q') # one ancille per factor
+	qr = QuantumRegister(n+1+d, 'q') # one aux for unitary embedding plus one aux per factor
 	circ = QuantumCircuit(n+1+d,n+1+d)
 	for i in range(n+1):
 		circ.h(qr[i])
-	
+
 	i = 0 # enumerate 0..d-1
 	for ii,L in enumerate(L0):
 		for jj,(w0,Phi0) in enumerate(L):
-			w = 0.5*w0 - (lnZ/len(C))
+			w = 0.5*w0 - (lnZ/len(C)) # 0.5 => sqrt(exp(..))
 			assert w < 0
-			
+
 			U = genUphi(uniEmbedding(Phi0), genPhaseFactors(np.exp(beta*w)))
-			
+
 			u = conjugateBlocks(U).to_circuit().to_instruction(label='U_C'+str(ii)+'_y'+str(jj))
-			
+
 			circ.h(qr[n+1+i])
 			circ.append(u, [qr[j] for j in range(n+1)]+[qr[n+1+i]])
 			circ.h(qr[n+1+i])
-			
+
 			i = i + 1
-			
+
 	circ.measure([qr[j] for j in range(n+1+d)],range(n+1+d))
 	return circ
 
 #######################################
 
-RUNS = [[[0]],[[0,1]],[[0,1],[1,2]],[[0,1],[1,2],[2,3]],[[0,1],[1,2],[2,3],[0,3]],[[0,1,2],[0,2,3]],[[0,1,2,3]]]
+#RUNS = [[[0]],[[0,1]],[[0,1],[1,2]],[[0,1],[1,2],[2,3]],[[0,1],[1,2],[2,3],[0,3]],[[0,1,2],[0,2,3]],[[0,1,2,3]]]
+RUNS = [[[0,1],[1,2],[2,3],[0,3]],[[0,1],[1,2],[2,3],[0,3],[3,4,5]]]
 
 logfile = open("results_experiment_1.csv", "w")
 
@@ -182,7 +183,7 @@ for C in RUNS:
 			if m > cmax:
 				cmax = m
 			d = d + (2**m)
-			
+
 		dim = 2**n
 
 		HAM,LL = genHamiltonian() # L is list of factors
@@ -191,14 +192,14 @@ for C in RUNS:
 		R2b = expH_from_list_real_RUS(beta, LL)
 		OL  = 3
 		UU  = transpile(R2b, basis_gates=['cx','id','rz','sx','x'], optimization_level=OL)
-		N   = 400000
+		N   = 1000000
 		sim = Aer.get_backend('aer_simulator')
 		j   = sim.run(assemble(UU,shots=N))
 		R   = j.result().get_counts()
 		Y   = list(itertools.product([0, 1], repeat=n))
 		P   = np.zeros(dim)
-		
-		
+
+
 		wmin = None
 		wmax = None
 		for l1 in LL:
@@ -207,7 +208,7 @@ for C in RUNS:
 					wmin = ww
 				elif ww < wmin:
 					wmin = ww
-				
+
 				if wmax is None:
 					wmax = ww
 				elif ww > wmax:
@@ -226,15 +227,15 @@ for C in RUNS:
 			print(logs)
 			logfile.write(logs+'\n')
 			continue
-		
+
 		ZZ = np.sum(P)
 		P = P/ZZ
 
 		lnZ = np.log(np.trace(R0))
 		Q = np.diag(R0/np.exp(lnZ))
-			
+
 		logs = str(n)+','+str(d)+','+str(len(C))+','+str(cmax)+','+str(np.real(fidelity(P,Q)))+','+str(ZZ/N)+','+str(len(UU))+','+str(UU.depth())+','+str(N)+','+str(wmin)+','+str(wmax)
 		print(logs)
 		logfile.write(logs+'\n')
-		
+
 logfile.close()
