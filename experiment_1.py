a9d42786212d08f35966afc4a97cc9d15636ec6e
Nico Piatkowski <np84>
2022-01-29 16:39:00 +0100

    added some comments

--- experiment_1.py
+++ experiment_1.py
@@ -129,7 +129,9 @@ def genUphi(U,phi):
 	print(phi)
 	RZ1 = (phi[0] * Z).exp_i() ^ (I^n) # ignored sign of phi
 	RZ2 = (phi[1] * Z).exp_i() ^ (I^n) # ignored sign of phi
-	return RZ1 @ U @ RZ2 @ U # ignored conjugate transpose of first U
+	assert phi[1] == phi[2]
+	assert U == ~U
+	return RZ1 @ U @ RZ1 @ U # ignored conjugate transpose of first U
 
 # returns unitary if Eigenvalues of A are bounded by 1
 def uniEmbedding(A):
