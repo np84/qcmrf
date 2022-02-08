cc93bc999bf7a9ee1df9c1944cbe37eb9d690c26
Nico Piatkowski <np84>
2022-02-08 13:00:01 +0100

    update

---
+++ qcmrf_delete.py
@@ -0,0 +1,7 @@
+import os
+from qiskit import IBMQ
+
+IBMQ.load_account()
+provider = IBMQ.get_provider(project='ticket')  # Substitute with your provider.
+
+provider.runtime.delete_program('qcmrf-??????')
--- qcmrf_upload.py
+++ qcmrf_upload.py
@@ -4,8 +4,8 @@ from qiskit import IBMQ
 IBMQ.load_account()
 provider = IBMQ.get_provider(project='ticket')  # Substitute with your provider.
 
-qcmrf_data = os.path.join(os.getcwd(), "./qcmrf_rt.py")
-qcmrf_json = os.path.join(os.getcwd(), "./qcmrf_rt.json")
+qcmrf_data = os.path.join(os.getcwd(), "./qcmrf_runtime.py")
+qcmrf_json = os.path.join(os.getcwd(), "./qcmrf_runtime.json")
 
 program_id = provider.runtime.upload_program(
     data=qcmrf_data,
